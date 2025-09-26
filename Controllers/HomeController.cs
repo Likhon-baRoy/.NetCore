c6b5fce1be8dbaf0f81462019b502174afd6aa4d
Likhon-baRoy <likhonhere007@proton.me>
2025-09-26 23:41:15 +0600

    multiple action method in Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,12 +5,25 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         // GET: HomeController
-        [Route("sayhello")] // one url for method1()
+        [Route("home")] // one url for method1()
         [Route("/")] // another url for method1()
-        public string method1()
+        public string Index()
         {
-            return "Hello from Controller method1()\n";
+            return "Hello from Index\n";
         }
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+
+        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")] // only Digit & exactly 10 number
+        public string Contact()
+        {
+            return "Hello from Contact";
+        }
+
         // public ActionResult Index()
         // {
         //     return View();
