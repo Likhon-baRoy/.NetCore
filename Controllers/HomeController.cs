af57d7e1b7c88ff729f016930b5a4e01fa436df3
Likhon-baRoy <likhonhere007@proton.me>
2025-09-27 03:17:58 +0600

    FileResult type of response

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,28 @@ namespace WebApp.Controllers
             return Json(person);
         }
 
+        [Route("file-download")]
+        public VirtualFileResult FileDownload() // if file is inside of wwwroot folder
+        {
+            // return new VirtualFileResult("/sample.pdf", "application/pdf");
+            return File("/sample.pdf", "application/pdf");
+        }
+
+        [Route("file-download2")]
+        public PhysicalFileResult FileDownload2() // if file is outside of wwwroot folder
+        {
+            // return new PhysicalFileResult("/home/likhon/likhon.pdf", "application/pdf"); // have to provide the full path
+            return PhysicalFile("/home/likhon/likhon.pdf", "application/pdf");
+        }
+
+        [Route("file-download3")]
+        public FileContentResult FileDownload3() // if is loaded from another data source as byte array as low level data
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes("/home/likhon/likhon.pdf");
+            // return new FileContentResult(bytes, "application/pdf");
+            return File(bytes, "application/pdf");
+        }
+
         [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")] // only Digit & exactly 10 number
         public string Contact()
         {
