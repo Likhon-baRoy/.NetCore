8f2848512eda7daa9f5c5fb78e65995d0f11608e
Likhon-baRoy <likhonhere007@proton.me>
2025-09-28 18:51:36 +0600

    Model Binding and Model State with validation Errors

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,35 +5,25 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // url: http://localhost:5049/bookstore/false/1?isloggedin=true&bookid=10
-
-        [Route("bookstore/{isloggedin?}/{bookid?}")]
-        public IActionResult Index(int? bookid, [FromRoute]bool? isloggedin, Book book)
+        [Route("register")]
+        public IActionResult Index(Person person)
         {
-            // Book Id should be supplied & can't be empty
-            if (bookid.HasValue == false)
+            if (!ModelState.IsValid)
             {
-                return BadRequest("Book id is not supplied or empty"); // Response.StatusCode = 400;
+                List<string> errorsList = new List<string>();
+
+                foreach (var values in ModelState.Values)
+                {
+                    foreach (var error in values.Errors)
+                    {
+                        errorsList.Add(error.ErrorMessage);
+                    }
+                }
+                string errors = string.Join('\n', errorsList);
+                return BadRequest(errors);
             }
 
-            // Book id should be between 1 to 1000
-            if (bookid <= 0)
-            {
-                return BadRequest("Book Id can't be less than or equal to zero");
-            }
-
-            if (bookid > 1000)
-            {
-                return NotFound("Book Id can't be greater than 1000"); // Response.StatusCode = 404;
-            }
-
-            // isloggedin should be true
-            if (isloggedin == false)
-            {
-                return Unauthorized("User must be authenticated"); // Response.StatusCode = 401;
-            }
-
-            return Content($"Book id: {bookid}, Book: {book}", "text/plain");
+            return Content($"{person}");
         }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp.Models;
 
 public class Person
 {
-  public Guid Id { get; set; }
-  public string? FirstName { get; set; }
-  public string? LastName { get; set; }
-  public int Age { get; set; }
+  [Required]
+  public string? PersonName { get; set; }
+  public string? Email { get; set; }
+  public string? Phone { get; set; }
+  public string? Password { get; set; }
+  public string? ConfirmPassword { get; set; }
+  public string? Price { get; set; }
+
+  public override string ToString()
+  {
+    return $"Person object -\nPerson Name: {PersonName}\nEmail: {Email}\nPhone: {Phone}\nPassword: {Password}\nConfirm Password: {ConfirmPassword}\nPrice: {Price}\n";
+  }
 }
