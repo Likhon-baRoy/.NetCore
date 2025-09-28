a6272420ae46d46d08a0293555b79c9d7da9849d
Likhon-baRoy <likhonhere007@proton.me>
2025-09-28 23:30:24 +0600

    all Model validations part-1

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,15 +1,20 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models;
 
 public class Person
 {
-  [Required(ErrorMessage = "Person Name can't be empty or null")]
+  [Required(ErrorMessage = "{0} can't be empty or null")]
+  [DisplayName("Person Name")]
+  [StringLength(40, MinimumLength = 4, ErrorMessage ="{0} should be between {2} to {1} characters long")]
   public string? PersonName { get; set; }
   public string? Email { get; set; }
   public string? Phone { get; set; }
   public string? Password { get; set; }
   public string? ConfirmPassword { get; set; }
+
+  [Range(0, 999.99, ErrorMessage = "{0} range should be between ${1} and ${2}")]
   public string? Price { get; set; }
 
   public override string ToString()
