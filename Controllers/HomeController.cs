9ac431c6436209f1cc306992ca535bc3fca9a942
Likhon-baRoy <likhonhere007@proton.me>
2025-10-05 19:45:28 +0600

    [FromBody] to get JSON data from request Body

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace WebApp.Controllers
     {
         [Route("register")]
         // public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))] Person person) // uses of Bind[] method
-        public IActionResult Index(Person person)
+        public IActionResult Index([FromBody] Person person)
         {
             if (!ModelState.IsValid)
             {
