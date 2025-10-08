111235bee6b9a7108ec4f1ee134c682cedf25b1e
Likhon-baRoy <likhonhere007@proton.me>
2025-10-08 07:31:14 +0600

    Strongly Typed Views - part: 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,5 +38,40 @@ namespace RazorView.Controllers
             return View(people);
         }
 
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            ViewBag.appTittle = "View Member Personal Details";
+
+            if (name == null)
+                return Content("Person name can't be null");
+
+            List<Person> people = new List<Person>()
+            {
+                new Person()
+                {
+                    Name = "John",
+                    DateOfBirth = DateTime.Parse("2000-05-06"),
+                    PersonGender = Gender.Male
+                },
+                new Person()
+                {
+                    Name = "Linda",
+                    DateOfBirth = DateTime.Parse("2000-01-09"),
+                    PersonGender = Gender.Female
+                },
+                new Person()
+                {
+                    Name = "Susan",
+                    DateOfBirth = null,
+                    PersonGender = Gender.Other
+                }
+            };
+
+            Person? matchingPerson = people.Where(temp => temp.Name == name).FirstOrDefault();
+
+            return View(matchingPerson);
+        }
+
     }
 }
