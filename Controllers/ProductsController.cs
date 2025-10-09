e1c7773f46d556fbdc28552d2b36a628a1edfd8c
Likhon-baRoy <likhonhere007@proton.me>
2025-10-09 17:54:55 +0600

    strongly typed partial views

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,18 +7,6 @@ namespace RazorView.Controllers
     [Route("products")]
     public ActionResult Index()
     {
-      ViewData["ListTitle"] = "Products";
-      ViewData["ListItems"] = new List<string>()
-      {
-        "ThinkPad Yoga",
-        "Andriod",
-        "Earphone",
-        "Mobile Charger",
-        "Wallet",
-        "Pen Driver",
-        "Water Bottle"
-      };
-
       return View();
     }
 
---
+++ Models/ListModel.cs
@@ -0,0 +1,7 @@
+namespace RazorView.Models;
+
+public class ListModel
+{
+  public string ListTitle { get; set; } = "";
+  public List<string> ListItems { get; set; } = new List<string>();
+}
