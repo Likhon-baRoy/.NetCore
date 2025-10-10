acb3c678faf918e5e7442005f09b6ebeff9d5639
Likhon-baRoy <likhonhere007@proton.me>
2025-10-10 09:32:38 +0600

    PartialView result dynamic loading using JS

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RazorView.Models;
 
 namespace RazorView.Controllers
 {
@@ -22,6 +23,26 @@ namespace RazorView.Controllers
     public ActionResult Order()
     {
       return View();
+    }
+
+    [Route("programming-languages")]
+    public IActionResult ProgrammingLanguages()
+    {
+      ListModel listModel = new ListModel()
+      {
+        ListTitle = "Programming Languages List",
+        ListItems = new List<string>()
+        {
+          "C",
+          "C#",
+          "C++",
+          "PHP",
+          "Java",
+          "Python"
+        }
+      };
+
+      return PartialView("_ListPartialView", listModel);
     }
   }
 }
