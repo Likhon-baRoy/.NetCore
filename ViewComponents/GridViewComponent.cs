aa74d5ec74fe1b3fa053f215aa71bb0cf37cbb07
Likhon-baRoy <likhonhere007@proton.me>
2025-10-11 17:03:21 +0600

    ViewComponents with Parameters

--- ViewComponents/GridViewComponent.cs
+++ ViewComponents/GridViewComponent.cs
@@ -5,30 +5,8 @@ namespace RazorView.ViewComponents;
 
 public class GridViewComponent : ViewComponent
 {
-  public async Task<IViewComponentResult> InvokeAsync()
+  public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
   {
-    PersonGridModel personGridModel = new PersonGridModel()
-    {
-      GridTitle = "Persons List from GridViewComponent",
-      Persons = new List<Person>()
-      {
-        new Person()
-        {
-          Name = "John",
-          JobTitle = "Manager"
-        },
-        new Person()
-        {
-          Name = "Jones",
-          JobTitle = "Asst. Manager"
-        },
-        new Person()
-        {
-          Name = "William",
-          JobTitle = "Clerk"
-        }
-      }
-    };
-    return View(personGridModel); /* Invoked a partial view & path is: Views/Shared/Components/Grid/Default.cshtml */
+    return View(grid); /* Invoked a partial view & path is: Views/Shared/Components/Grid/Default.cshtml */
   }
 }
