625d543655fca12a2c3bd12d809a7c0d7c804023
Likhon-baRoy <likhonhere007@proton.me>
2025-10-18 08:53:18 +0600

    add Configuration as Service, this is more cleaner

--- RazorView/Controllers/ConfigController.cs
+++ RazorView/Controllers/ConfigController.cs
@@ -1,30 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using RazorView.Models;
 
 namespace RazorView.Controllers
 {
     public class ConfigController : Controller
     {
-        private readonly IConfiguration _configuration;
+        private readonly WeatherApiOptions _options;
 
-        public ConfigController(IConfiguration configuration)
+        public ConfigController(IOptions<WeatherApiOptions> weatherApiOptions)
         {
-            _configuration = configuration;
+            _options = weatherApiOptions.Value;
         }
 
         [Route("config")]
         public ActionResult Index()
         {
-            // using options object to get specific keys
-            // Options: Loads configuration values into a new Options object
-            WeatherApiOptions options = _configuration.GetSection("weatherAPI").Get<WeatherApiOptions>();
-
-            // Bind: Loads configuration values into a new Options object
-            // WeatherApiOptions options = new WeatherApiOptions();
-            // _configuration.GetSection("weatherAPI").Bind(options);
-
-            ViewBag.ClientID = options.ClientID;
-            ViewBag.ClientSecret = options.ClientSecret;
+            ViewBag.ClientID = _options.ClientID;
+            ViewBag.ClientSecret = _options.ClientSecret;
 
             return View();
         }
--- RazorView/Program.cs
+++ RazorView/Program.cs
@@ -1,8 +1,13 @@
+using RazorView.Models;
 using ServiceContracts;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+// supply an Object of WeatherApiOptions (with 'weatherapi' section) as a service
+builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherAPI"));
+
 // builder.Services.AddTransient<ICitiesService, CitiesService>();
 builder.Services.AddScoped<ICitiesService, CitiesService>();
 // builder.Services.AddSingleton<ICitiesService, CitiesService>();
