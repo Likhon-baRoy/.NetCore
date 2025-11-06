831f11ba2698f939f178239c141f8ce42721f745
Likhon-baRoy <likhonhere007@proton.me>
2025-11-06 17:40:04 +0600

    search in Persons List view

--- RazorView/Controllers/PersonsController.cs
+++ RazorView/Controllers/PersonsController.cs
@@ -15,11 +15,23 @@ namespace RazorView.Controllers
         }
 
         [Route("persons/index")]
-        public ActionResult Index()
+        public ActionResult Index(string searchBy, string? searchString)
         {
-            List<PersonResponse> persons = _personsService.GetAllPersons();
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Emal" },
+                { nameof(PersonResponse.DateOfBirth), "Date Of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryID), "Country" },
+                { nameof(PersonResponse.Address), "Address" }
+            };
+
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+
+            List<PersonResponse> persons = _personsService.GetFilteredPersons(searchBy, searchString);
             return View(persons);
         }
-
     }
 }
